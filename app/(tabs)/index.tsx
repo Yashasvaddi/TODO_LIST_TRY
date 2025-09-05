239b2469e97d82508a099c1eef76ade43e842c09
Mandar Ekal <mandarekal367@gmail.com>
2025-09-06 00:19:12 +0530

    test

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-blue-200">
-      <Text className="text-2xl font-bold text-red-700">
+      <Text className="text-4xl font-bold text-red-700">
         Tailwind + Drawer + Expo Router 🚀
       </Text>
     </View>
