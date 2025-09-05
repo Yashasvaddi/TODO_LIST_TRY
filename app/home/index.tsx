4842499b37aae0ee4308151ae0cadf5fde84a221
Mandar Ekal <mandarekal367@gmail.com>
2025-09-06 00:35:32 +0530

    boilerplate

---
+++ app/home/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-100">
+      <Text className="text-2xl font-bold text-blue-800">
+        Home Screen 🏠
+      </Text>
+    </View>
+  );
+}
